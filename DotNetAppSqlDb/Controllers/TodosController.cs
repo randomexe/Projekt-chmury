5eda794cca62212fd7ec211dfe6b3a2990bab69a
lukaj <lukaj@LAPTOP-BPUTM133>
2022-01-15 23:18:57 +0100

    Dodaj pliki projektów.

---
+++ DotNetAppSqlDb/Controllers/TodosController.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DotNetAppSqlDb.Models;using System.Diagnostics;
+
+namespace DotNetAppSqlDb.Controllers
+{
+    public class TodosController : Controller
+    {
+        private MyDatabaseContext db = new MyDatabaseContext();
+
+
+        public ActionResult Index()
+        {            
+            Trace.WriteLine("GET /Todos/Index");
+            return View(db.Todoes.ToList());
+        }
+
+
+        public ActionResult Details(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Details/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+
+        public ActionResult Create()
+        {
+            Trace.WriteLine("GET /Todos/Create");
+            return View(new Todo { CreatedDate = DateTime.Now });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Description,CreatedDate")] Todo todo)
+        {
+            Trace.WriteLine("POST /Todos/Create");
+            if (ModelState.IsValid)
+            {
+                db.Todoes.Add(todo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Edit/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Description,CreatedDate")] Todo todo)
+        {
+            Trace.WriteLine("POST /Todos/Edit/" + todo.ID);
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Delete/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trace.WriteLine("POST /Todos/Delete/" + id);
+            Todo todo = db.Todoes.Find(id);
+            db.Todoes.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DotNetAppSqlDb/Models/MyDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+
+    
+        public MyDatabaseContext() : base("name=MyDbConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<DotNetAppSqlDb.Models.Todo> Todoes { get; set; }
+    }
+}
